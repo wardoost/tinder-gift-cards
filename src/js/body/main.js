598b1bb929e6167feaa1695eb408463c3a979be6
driesdepoorter <mail@driesdepoorter.be>
2015-12-21 23:43:42 +0100

    Clean up generatePDF function

--- src/js/body/main.js
+++ src/js/body/main.js
@@ -21,7 +21,9 @@ var init = function(){
     });
 
   $("#generateBtn").on('click', function(e) {
-    generatePDF();
+
+    var tinderUsername = "driesdepoorter"
+    generatePDF(tinderUsername);
     console.log("Base64test: " + getBase64("../img/header.jpg"));
    
   });
@@ -29,23 +31,24 @@ var init = function(){
 
 
 
-var generatePDF = function(){
+var generatePDF = function(tinderUsername){
 
-  console.log("-- STARTER GENERATING PDF --");
+  console.log("-- START GENERATING PDF --");
   // You'll need to make your image into a Data URL
   // Use http://dataurl.net/#dataurlmaker
-  //var imgData = getBase64Image("http://images.gotinder.com/52e418a804580e204b00033c/640x640_e73edb75-10a3-4112-8ca5-fee6d332a722.jpg")
+  var imgData = getBase64("http://images.gotinder.com/52e418a804580e204b00033c/640x640_e73edb75-10a3-4112-8ca5-fee6d332a722.jpg");
   var doc = new jsPDF();
+  
+  console.log("-- START GENERATING TINDER LINK  -- ");
   doc.setFontSize(40);
-  doc.text(35, 25, "Paranyan loves jsPDF");
+  doc.text(35, 25, "www.tinder.com/@" + tinderUsername);
   //doc.addImage(imgData, 'JPEG', 15, 40, 180, 160);
 
   console.log("-- START GENERATING QR -- ");
-  var dataUrl = qr.toDataURL({ mime: 'image/jpeg', value: 'driesdepoorter' }); 
-  console.log(dataUrl);
-  doc.addImage(dataUrl,'JPEG',15,40,180,160);
+  var dataUrl = qr.toDataURL({ mime: 'image/jpeg', value: tinderUsername }); 
+  doc.addImage(dataUrl,'JPEG',15,40,40,40);
 
-  doc.save('test.pdf');
+  doc.save('tinderMe Cards - ' + tinderUsername + '.pdf');
 }
 
 var getBase64 = function (url) {
@@ -59,6 +62,31 @@ var getBase64 = function (url) {
   var dataURL = canvas.toDataURL("image/png");  //This line of code will throw exception
   return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 }
+
+
+function getBase64FromImageUrl(url) {
+    var img = new Image();
+
+    img.setAttribute('crossOrigin', 'anonymous');
+
+    img.onload = function () {
+        var canvas = document.createElement("canvas");
+        canvas.width =this.width;
+        canvas.height =this.height;
+
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(this, 0, 0);
+
+        var dataURL = canvas.toDataURL("image/png");
+
+        alert(dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
+    };
+
+    img.src = url;
+}
+
+
+
 
 
 // ---------------------------------------------
