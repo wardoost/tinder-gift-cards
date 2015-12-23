26af264c33fe92b2c50ed0d3701d86c65bea97dd
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2015-12-23 19:40:43 +0100

    Form update

--- src/js/body/main.js
+++ src/js/body/main.js
@@ -8,7 +8,7 @@ var init = function(){
      evt.preventDefault(); // prevent default anchor click behavior
      var hash = this.hash; // store hash
      // animate scroll
-     $('html, body').animate({
+     $("html, body").animate({
          scrollTop: $(hash).offset().top
        }, 300, function(){
          // when done, add hash to url (default click behaviour)
@@ -17,14 +17,14 @@ var init = function(){
   });
   // Collapse menu on anchor links in navbar
   $(".navbar-nav li a[href^='#']").click(function(event) {
-      $(".navbar-collapse").collapse('hide');
+      $(".navbar-collapse").collapse("hide");
     });
 
-  $("#generateBtn").on('click', function(e) {
+  $("#generateBtn").on("click", function(e) {
 
-    var tinderUsername = "driesdepoorter"
+    var tinderUsername = $("#inputTinderUsername").val();
     generatePDF(tinderUsername);
-    console.log("Base64test: " + getBase64("../img/header.jpg"));
+    //console.log("Base64test: " + getBase64("../img/header.jpg"));
    
   });
 }
@@ -33,27 +33,22 @@ var init = function(){
 
 var generatePDF = function(tinderUsername){
 
-  console.log("-- START GENERATING PDF --");
   // You'll need to make your image into a Data URL
   // Use http://dataurl.net/#dataurlmaker
-  var imgData = getBase64("http://images.gotinder.com/52e418a804580e204b00033c/640x640_e73edb75-10a3-4112-8ca5-fee6d332a722.jpg");
   var doc = new jsPDF();
   
-  console.log("-- START GENERATING TINDER LINK  -- ");
   doc.setFontSize(40);
   doc.text(35, 25, "www.tinder.com/@" + tinderUsername);
-  //doc.addImage(imgData, 'JPEG', 15, 40, 180, 160);
 
-  console.log("-- START GENERATING QR -- ");
-  var dataUrl = qr.toDataURL({ mime: 'image/jpeg', value: tinderUsername }); 
-  doc.addImage(dataUrl,'JPEG',15,40,40,40);
+  var dataUrl = qr.toDataURL({ mime: "image/jpeg", value: tinderUsername }); 
+  doc.addImage(dataUrl,"JPEG",15,40,40,40);
 
-  doc.save('tinderMe Cards - ' + tinderUsername + '.pdf');
+  doc.save("TinderMe-Card-" + tinderUsername + ".pdf");
 }
 
 var getBase64 = function (url) {
   var img = document.getElementById("profilePic");
-  //img.setAttribute('crossOrigin', 'anonymous');
+  //img.setAttribute("crossOrigin", "anonymous");
   var canvas = document.createElement("canvas");
   canvas.width = img.width;
   canvas.height = img.width;
@@ -62,32 +57,6 @@ var getBase64 = function (url) {
   var dataURL = canvas.toDataURL("image/png");  //This line of code will throw exception
   return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 }
-
-
-function getBase64FromImageUrl(url) {
-    var img = new Image();
-
-    img.setAttribute('crossOrigin', 'anonymous');
-
-    img.onload = function () {
-        var canvas = document.createElement("canvas");
-        canvas.width =this.width;
-        canvas.height =this.height;
-
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(this, 0, 0);
-
-        var dataURL = canvas.toDataURL("image/png");
-
-        alert(dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
-    };
-
-    img.src = url;
-}
-
-
-
-
 
 // ---------------------------------------------
 // INITIATION ON PAGE LOAD
