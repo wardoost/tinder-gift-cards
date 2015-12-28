4a42156fa4f9d4bcfd936dfbde5fe2249bd157bb
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2015-12-28 18:22:57 +0100

    Profile pic in PDF

--- src/js/body/main.js
+++ src/js/body/main.js
@@ -4,11 +4,11 @@
 var init = function(){
 
   // Anchor links animate scroll
-  $("a[href^='#']").click(function(e) {
+  $('a[href^="#"]').click(function(e) {
      e.preventDefault(); // prevent default anchor click behavior
      var hash = this.hash; // store hash
      // animate scroll
-     $("html, body").animate({
+     $('html, body').animate({
          scrollTop: $(hash).offset().top
        }, 300, function(){
          // when done, add hash to url (default click behaviour)
@@ -16,23 +16,23 @@ var init = function(){
        });
   });
   // Collapse menu on anchor links in navbar
-  $(".navbar-nav li a[href^='#']").click(function(e) {
-      $(".navbar-collapse").collapse("hide");
+  $('.navbar-nav li a[href^="#"]').click(function(e) {
+      $('.navbar-collapse').collapse('hide');
     });
 
   // Generate PDF button
-  $("#generateBtn").on("click", function(e) {
-    getUserData($("#inputTinderUsername").val());
+  $('#generateBtn').on('click', function(e) {
+    getUserData($('#inputTinderUsername').val());
   });
 }
 
 var getUserData = function(tinderUsername){
 
-  var webProfileURL = "http://www.gotinder.com/@" + tinderUsername;
+  var webProfileURL = 'http://www.gotinder.com/@' + tinderUsername;
 
   // Get info from tinder web profile page
   $.ajax({
-    url: "php/scraping_tinder.php?url=" + webProfileURL,
+    url: 'php/scraping_tinder.php?url=' + webProfileURL,
     type: 'get',
     dataType: 'html',
     async: false,
@@ -40,35 +40,38 @@ var getUserData = function(tinderUsername){
       var result = jQuery.parseJSON(data);
 
       var name = result.name;
-      var age = result.age.replace(/&nbsp;/g, "");
-      var img = result.img.replace(/&#x2F;/g, "/");
+      var age = result.age.replace(/&nbsp;/g, '');
+      var imgData = 'data:image/jpeg;base64,' + result.imgdata;
 
-      //$("#profilePic").attr("src", img);
-      //$("#profile").fadeIn(300);
-      generatePDF(webProfileURL, tinderUsername, name, age, img);
+      // Generate PDF with user info
+      generatePDF(webProfileURL, tinderUsername, name, age, imgData);
     },
     error: function(){
+      // Generate PDF with only the web profile link
       generateAltPDF(webProfileURL);
     }
   });
 }
 
-var generatePDF = function(url, username, name, age, img){
+var generatePDF = function(url, username, name, age, imgData){
   // Create PDF
   var doc = new jsPDF();
 
   // Add user data 
   doc.setFontSize(20);
-  doc.text(20, 20, url.replace("http://", ""));
+  doc.text(20, 20, url.replace('http://', ''));
   doc.text(20, 30, name);
   doc.text(20, 40, age);
 
   // Add QR from web profile URL
-  var qrcode = qr.toDataURL({ mime: "image/jpeg", value: url, background: '#FFFFFF', foreground: '#DC6639' }); 
-  doc.addImage(qrcode,"JPEG",20,50,40,40);
+  var qrcode = qr.toDataURL({ mime: 'image/jpeg', value: url, background: '#FFFFFF', foreground: '#DC6639' }); 
+  doc.addImage(qrcode,'JPEG',20,50,40,40);
+
+  // Add web profile image
+  doc.addImage(imgData, 'JPEG', 20, 100, 100, 100);
 
   // Download/open PDF depending on browser settings
-  doc.save("TinderMe-Card-" + username + ".pdf");
+  doc.save('TinderMe-Card-' + username + '.pdf');
 }
 
 var generateAltPDF = function(url){
@@ -77,26 +80,14 @@ var generateAltPDF = function(url){
 
   // Add user data
   doc.setFontSize(20);
-  doc.text(20, 20, url.replace("http://", ""));
+  doc.text(20, 20, url.replace('http://', ''));
 
   // Add QR from web profile URL
-  var qrcode = qr.toDataURL({ mime: "image/jpeg", value: url, background: '#FFFFFF', foreground: '#DC6639' }); 
-  doc.addImage(qrcode,"JPEG",20,50,40,40);
+  var qrcode = qr.toDataURL({ mime: 'image/jpeg', value: url, background: '#FFFFFF', foreground: '#DC6639' }); 
+  doc.addImage(qrcode,'JPEG',20,50,40,40);
 
   // Download/open PDF depending on browser settings
-  doc.save("TinderMe-Card-" + username + ".pdf");
-}
-
-var getBase64 = function (url) {
-  var img = document.getElementById("profilePic");
-  //img.setAttribute("crossOrigin", "anonymous");
-  var canvas = document.createElement("canvas");
-  canvas.width = img.width;
-  canvas.height = img.width;
-  var ctx = canvas.getContext("2d");
-  ctx.drawImage(img, 0, 0);
-  var dataURL = canvas.toDataURL("image/png");  //This line of code will throw exception
-  return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+  doc.save('TinderMe-Card-' + username + '.pdf');
 }
 
 // ---------------------------------------------
--- src/php/scraping_tinder.php
+++ src/php/scraping_tinder.php
@@ -11,7 +11,8 @@ $data['name'] = $html->find('title', 0)->innertext;
 $data['age'] = $html->find('div[id="info-container"] span[id="age"]', 0)->innertext;
 
 // get profile image
-$data['img'] = $html->find('img[id="user-photo"]', 0)->src;
+$data['imgsrc'] = str_replace("&#x2F;", "/", $html->find('img[id="user-photo"]', 0)->src);
+$data['imgdata'] = base64_encode(file_get_contents($data['imgsrc']));
 
 // clean up memory
 $html->clear();
